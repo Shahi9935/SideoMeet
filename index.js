e5e0b74feaa9eadba938c5c3c87c3c7a86a2f2df
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2018-08-17 20:25:50 +0530

    Test

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const  express = require('express');
 const app = express();
 
 // Set public folder as root
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // Provide access to node_modules folder from the client-side
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Redirect all traffic to index.html
-app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.get('/',()=>{
+console.log("HEY");
+});
 
 app.listen(process.env.PORT||3000, () => {
   console.info(`Server started`);
